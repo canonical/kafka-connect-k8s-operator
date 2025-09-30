0dd9624e57d931f5c01b32be2d4bbc8413dbb881
Iman Enami <iman.enami@canonical.com>
2025-09-30 10:29:16 +0200

    fix: add trust

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,18 @@
 resource "juju_application" "connect" {
   model = var.model
   name  = var.app_name
-  
+
   charm {
     name     = "kafka-connect-k8s"
     channel  = var.channel
     revision = var.revision
     base     = var.base
   }
-  
+
   units       = var.units
   constraints = var.constraints
   config      = var.config
+  trust       = true
 }
 
 resource "juju_offer" "connect_client" {
