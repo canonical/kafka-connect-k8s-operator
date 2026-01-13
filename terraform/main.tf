8fb76d5ea375b50a2609898b01775240e2ebca1f
Iman Enami <44609233+imanenami@users.noreply.github.com>
2026-01-13 10:00:28 +0400

    [DPE-9033] chore: update TF provider to v1.0+ and TLS to 1/stable (#17)

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 resource "juju_application" "connect" {
-  model = var.model
-  name  = var.app_name
+  model_uuid = var.model_uuid
+  name       = var.app_name
 
   charm {
     name     = "kafka-connect-k8s"
@@ -16,7 +16,7 @@ resource "juju_application" "connect" {
 }
 
 resource "juju_offer" "connect_client" {
-  model            = var.model
+  model_uuid       = var.model_uuid
   application_name = juju_application.connect.name
   endpoints        = ["connect-client"]
 }
